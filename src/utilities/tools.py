f3a3ce625edae5ca855e05cc4ffdfcd321b865be
falimoradi <47635556+falimoradi@users.noreply.github.com>
2023-03-01 10:23:04 +0330

    Update tools.py

--- src/utilities/tools.py
+++ src/utilities/tools.py
@@ -211,7 +211,8 @@ def generate_mask_uplft(input_image, window_shape, upper_left_points, gpu_number
     :param upper_left_points:
     """
     
-    print('****************', window_shape.is_cuda)
+    print('****************', input_image.is_cuda)
+    print('****************', window_shape[0].is_cuda)
     
     N, C, H, W = input_image.size()
     window_h, window_w = window_shape
